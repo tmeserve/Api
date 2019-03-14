89a8e7908f2f482da6fc9196e1d37287d908fd4c
Tyler Meserve <31040347+tmeserve@users.noreply.github.com>
2019-03-14 11:43:15 -0500

    Initial Commit

--- server.py
+++ server.py
@@ -1,1 +1,230 @@
+"""Python Flask API Auth0 integration example
+"""
 
+from functools import wraps
+import json
+from os import environ as env
+from six.moves.urllib.request import urlopen
+
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask, request, jsonify, _request_ctx_stack
+from flask_cors import cross_origin
+from jose import jwt
+from flask_pymongo import PyMongo
+
+from collections import defaultdict
+
+from flask_cors import CORS
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+AUTH0_DOMAIN = env.get("AUTH0_DOMAIN")
+API_IDENTIFIER = env.get("API_IDENTIFIER")
+ALGORITHMS = ["RS256"]
+APP = Flask(__name__)
+APP.debug = True
+APP.config['MONGO_DBNAME'] = 'test_rest'
+APP.config['MONGO_URI'] = 'mongodb://localhost:27017/test_rest'
+mongo = PyMongo(APP)
+CORS(APP)
+
+
+# Format error response and append status code.
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
+@APP.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
+
+
+def get_token_auth_header():
+    """Obtains the access token from the Authorization Header
+    """
+    auth = request.headers.get("Authorization", None)
+    if not auth:
+        raise AuthError({"code": "authorization_header_missing",
+                        "description":
+                            "Authorization header is expected"}, 401)
+
+    parts = auth.split()
+
+    if parts[0].lower() != "bearer":
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Authorization header must start with"
+                            " Bearer"}, 401)
+    elif len(parts) == 1:
+        raise AuthError({"code": "invalid_header",
+                        "description": "Token not found"}, 401)
+    elif len(parts) > 2:
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Authorization header must be"
+                            " Bearer token"}, 401)
+
+    token = parts[1]
+    return token
+
+
+def requires_scope(required_scope):
+    """Determines if the required scope is present in the access token
+    Args:
+        required_scope (str): The scope required to access the resource
+    """
+    token = get_token_auth_header()
+    unverified_claims = jwt.get_unverified_claims(token)
+    if unverified_claims.get("scope"):
+        token_scopes = unverified_claims["scope"].split()
+        for token_scope in token_scopes:
+            if token_scope == required_scope:
+                return True
+    return False
+
+
+def requires_auth(f):
+    """Determines if the access token is valid
+    """
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = get_token_auth_header()
+        jsonurl = urlopen("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json")
+        jwks = json.loads(jsonurl.read())
+        try:
+            unverified_header = jwt.get_unverified_header(token)
+        except jwt.JWTError:
+            print("exc 1")
+            raise AuthError({"code": "invalid_header",
+                            "description":
+                                "Invalid header. "
+                                "Use an RS256 signed JWT Access Token"}, 401)
+        if unverified_header["alg"] == "HS256":
+            print("exc 2")
+            raise AuthError({"code": "invalid_header",
+                            "description":
+                                "Invalid header. "
+                                "Use an RS256 signed JWT Access Token"}, 401)
+        rsa_key = {}
+        for key in jwks["keys"]:
+            if key["kid"] == unverified_header["kid"]:
+                rsa_key = {
+                    "kty": key["kty"],
+                    "kid": key["kid"],
+                    "use": key["use"],
+                    "n": key["n"],
+                    "e": key["e"]
+                }
+        if rsa_key:
+            try:
+                payload = jwt.decode(
+                    token,
+                    rsa_key,
+                    algorithms=ALGORITHMS,
+                    audience=API_IDENTIFIER,
+                    issuer="https://"+AUTH0_DOMAIN+"/"
+                )
+            except jwt.ExpiredSignatureError:
+                print("Exc 1")
+                raise AuthError({"code": "token_expired",
+                                "description": "token is expired"}, 401)
+            except jwt.JWTClaimsError:
+                print("Exc 2")
+                raise AuthError({"code": "invalid_claims",
+                                "description":
+                                    "incorrect claims,"
+                                    " please check the audience and issuer"}, 401)
+            except Exception:
+                print("Exception 3")
+                raise AuthError({"code": "invalid_header",
+                                "description":
+                                    "Unable to parse authentication"
+                                    " token."}, 401)
+
+            _request_ctx_stack.top.current_user = payload
+            return f(*args, **kwargs)
+        raise AuthError({"code": "invalid_header",
+                        "description": "Unable to find appropriate key"}, 401)
+    return decorated
+
+@APP.route('/api/usernames', methods=['GET'])
+@cross_origin(headers=['Content-Type', 'Authorization'])
+@cross_origin(headers=["Access-Control-Allow-Origin", "http://localhost:4200"])
+@requires_auth
+def get_all_usernames():
+    username = mongo.db.usernames
+    output = {"name": []}
+    for s in username.find():
+        output["name"].append(s["name"])
+
+    return jsonify(output)
+
+@APP.route('/api/usernames/', methods=['GET'])
+@cross_origin(headers=['Content-Type', 'Authorization'])
+@cross_origin(headers=["Access-Control-Allow-Origin", "http://localhost:4200"])
+@requires_auth
+def get_one_username(name):
+    username = mongo.db.usernames
+    u = username.find_one({'name' : name})
+    if u:
+        output = {'name' : u['name']}
+    else:
+        output = "No such name"
+    return jsonify(str(output))
+
+@APP.route('/api/usernames', methods=['POST'])
+@cross_origin(headers=['Content-Type', 'Authorization'])
+@cross_origin(headers=["Access-Control-Allow-Origin", "http://localhost:4200"])
+@requires_auth
+def add_username():
+    username = mongo.db.usernames
+    name = request.json['name']
+    user_id = username.insert({'name': name})
+    new_user = username.find_one({'_id': user_id})
+    output = {'name' : new_user['name']}
+    return jsonify(output)
+
+# Controllers API
+@APP.route("/api/public")
+@cross_origin(headers=["Content-Type", "Authorization"])
+def public():
+    """No access token required to access this route
+    """
+    response = "Hello from a public endpoint! You don't need to be authenticated to see this."
+    return jsonify(message=response)
+
+
+@APP.route("/api/private")
+@cross_origin(headers=["Content-Type", "Authorization"])
+@cross_origin(headers=["Access-Control-Allow-Origin", "http://127.0.0.1:4200"])
+@requires_auth
+def private():
+    """A valid access token is required to access this route
+    """
+    response = "Hello from a private endpoint! You need to be authenticated to see this."
+    return jsonify(message=response)
+
+
+@APP.route("/api/private-scoped")
+@cross_origin(headers=["Content-Type", "Authorization"])
+@cross_origin(headers=["Access-Control-Allow-Origin", "http://127.0.0.1:4200"])
+@requires_auth
+def private_scoped():
+    """A valid access token and an appropriate scope are required to access this route
+    """
+    if requires_scope("read:messages"):
+        response = "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
+        return jsonify(message=response)
+    raise AuthError({
+        "code": "Unauthorized",
+        "description": "You don't have access to this resource"
+    }, 403)
+
+
+if __name__ == "__main__":
+    APP.run(host="0.0.0.0", port=env.get("PORT", 3010))
